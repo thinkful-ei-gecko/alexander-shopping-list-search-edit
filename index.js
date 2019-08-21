303b4a5cf9c0d09ecfe2c638d6bc43348682a996
Alexander Fukui <alexander.fukui@gmail.com>
2019-08-20 20:22:17 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
 
-const STORE = [
-  {id: cuid(), name: "apples", checked: false},
-  {id: cuid(), name: "oranges", checked: false},
-  {id: cuid(), name: "milk", checked: true},
-  {id: cuid(), name: "bread", checked: false}
-];
-
+const STORE = {
+  items: [
+    {id: cuid(), name: "apples", checked: false},
+    {id: cuid(), name: "oranges", checked: false},
+    {id: cuid(), name: "milk", checked: true},
+    {id: cuid(), name: "bread", checked: false}
+  ],
+  hideCompleted: false
+};
 
 function generateItemElement(item) {
   return `
@@ -18,6 +20,9 @@ function generateItemElement(item) {
         </button>
         <button class="shopping-item-delete js-item-delete">
             <span class="button-label">delete</span>
+        </button>
+        <button class="shopping-item-edit js-item-edit">
+        <span class="button-label">edit</span>
         </button>
       </div>
     </li>`;
@@ -33,37 +38,140 @@ function generateShoppingItemsString(shoppingList) {
 }
 
 
-function renderShoppingList() {
+function render(query) {
   // render the shopping list in the DOM
-  console.log('`renderShoppingList` ran');
-  const shoppingListItemsString = generateShoppingItemsString(STORE);
+  console.log('`render` ran');
+
+  // set up a copy of the store's items in a local variable that we will reassign to a new
+  // version if any filtering of the list occurs
+  let filteredItems = STORE.items;
+
+  // if the `hideCompleted` property is true, then we want to reassign filteredItems to a version
+  // where ONLY items with a "checked" property of false are included
+  if (STORE.hideCompleted) {
+    filteredItems = filteredItems.filter(item => !item.checked);
+  }
+
+  if (query) {
+    filteredItems = filteredItems.filter(item => item.name === query)
+  }
+
+  // at this point, all filtering work has been done (or not done, if that's the current settings), so
+  // we send our `filteredItems` into our HTML generation function 
+  const shoppingListItemsString = generateShoppingItemsString(filteredItems);
 
   // insert that HTML into the DOM
   $('.js-shopping-list').html(shoppingListItemsString);
 }
 
 
+function addItemToShoppingList(itemName) {
+  console.log(`Adding "${itemName}" to shopping list`);
+  STORE.items.push({name: itemName, checked: false});
+}
+
 function handleNewItemSubmit() {
-  // this function will be responsible for when users add a new shopping list item
-  $('.js-shopping-list-form').submit(event => {
-    event.preventDefault;
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    console.log('`handleNewItemSubmit` ran');
+    const newItemName = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+    addItemToShoppingList(newItemName);
+    render();
   });
-  
-  console.log('`handleNewItemSubmit` ran');
+}
+
+function toggleCheckedForListItem(itemId) {
+  console.log("Toggling checked property for item with id " + itemId);
+  const item = STORE.items.find(item => item.id === itemId);
+  item.checked = !item.checked;
 }
 
 
+function getItemIdFromElement(item) {
+  return $(item)
+    .closest('li')
+    .data('item-id');
+}
+
 function handleItemCheckClicked() {
-  // this function will be responsible for when users click the "check" button on
-  // a shopping list item.
-  console.log('`handleItemCheckClicked` ran');
+  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
+    console.log('`handleItemCheckClicked` ran');
+    const id = getItemIdFromElement(event.currentTarget);
+    toggleCheckedForListItem(id);
+    render();
+  });
+}
+
+
+// name says it all. responsible for deleting a list item.
+function deleteListItem(itemId) {
+  console.log(`Deleting item with id  ${itemId} from shopping list`)
+
+  // as with `addItemToShoppingLIst`, this function also has the side effect of
+  // mutating the global STORE value.
+  //
+  // First we find the index of the item with the specified id using the native
+  // Array.prototype.findIndex() method. Then we call `.splice` at the index of 
+  // the list item we want to remove, with a removeCount of 1.
+  const itemIndex = STORE.items.findIndex(item => item.id === itemId);
+  STORE.items.splice(itemIndex, 1);
 }
 
 
 function handleDeleteItemClicked() {
-  // this function will be responsible for when users want to delete a shopping list
-  // item
-  console.log('`handleDeleteItemClicked` ran')
+  // like in `handleItemCheckClicked`, we use event delegation
+  $('.js-shopping-list').on('click', '.js-item-delete', event => {
+    // get the index of the item in STORE
+    const itemIndex = getItemIdFromElement(event.currentTarget);
+    // delete the item
+    deleteListItem(itemIndex);
+    // render the updated shopping list
+    render();
+  });
+}
+
+// Toggles the STORE.hideCompleted property
+function toggleHideFilter() {
+  STORE.hideCompleted = !STORE.hideCompleted;
+}
+
+// Places an event listener on the checkbox for hiding completed items
+function handleToggleHideFilter() {
+  $('.js-hide-completed-toggle').on('click', () => {
+    toggleHideFilter();
+    render();
+  });
+}
+
+//Listen for search form submission.
+function handleSearchSubmit(){
+  $('#js-search-form').submit( event => {
+    event.preventDefault();
+    // Get value of search input.
+    const query = $('.js-search-input').val();
+    // Run query through render filter function which is modified to return filtered array
+    render(query);
+  });
+  console.log('`handleSearchClicked` ran');
+}
+
+function handleEditClicked() {
+  $('.js-shopping-list').on('click', '.js-item-edit', event => {
+    console.log('`handleEditClicked` ran.');
+    const newName = prompt('Enter New Name', '');
+    const id = getItemIdFromElement(event.currentTarget);
+    replaceListItemName(id, newName);
+    render();
+  }
+  );
+}
+
+function replaceListItemName(itemId, newName) {
+  //Locates item's array index in STORE.
+  const itemIndex = STORE.items.findIndex(item => item.id === itemId);
+  //Refer to item by index to change name.
+  STORE.items[itemIndex].name = newName;
 }
 
 // this function will be our callback when the page loads. it's responsible for
@@ -71,11 +179,13 @@ function handleDeleteItemClicked() {
 // that handle new item submission and user clicks on the "check" and "delete" buttons
 // for individual shopping list items.
 function handleShoppingList() {
-  renderShoppingList();
+  render();
   handleNewItemSubmit();
   handleItemCheckClicked();
   handleDeleteItemClicked();
-
+  handleToggleHideFilter();
+  handleSearchSubmit();
+  handleEditClicked();
 }
 
 // when the page loads, call `handleShoppingList`
